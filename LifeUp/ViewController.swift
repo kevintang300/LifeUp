9fd9bd9e3e33aa307bc0e803b64db6bfc2f06bf6
Kevin Tang <kevintang300@gmail.com>
2020-06-27 01:47:51 -0700

    Initial Commit

---
+++ LifeUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeUp
+//
+//  Created by Kevin Tang on 6/27/20.
+//  Copyright © 2020 KeTapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
